6e6e6e71302785cf74d11f5e47a64a892edc4000
Johan Reyes <142470650+Zombender@users.noreply.github.com>
2024-12-06 18:10:57 -0600

    Eliminada referencia de constante de CIF y PIN

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
 UAM_BIBLIO_API = 'https://uvirtual.uam.edu.ni:442/uambiblioapi/User/'
+
 UAM_BIBLIO_POST_HEADER = {
     "Content-Type": "application/json",
     "X-Uam-Secure-Api": "version-1.0"
 }
+
 def get_uam_biblio_get_header(token:str):
     return {
     "Accept": "application/json",
@@ -11,6 +13,4 @@ def get_uam_biblio_get_header(token:str):
     "Authorization": token
     }
 
-STUDENT_CIF = ''
-STUDENT_PIN = ''
 
